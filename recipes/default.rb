a8c27702810bfc47765d2f18bb2dcf0ab8835b22
Nikita Simakov <everm1nd.mail@gmail.com>
2012-03-28 14:12:22 +0400

    rbenv doesn't see /etc/gemrc fixed with symlink at /home/#{user}/.gemrc

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,4 +8,11 @@
 #
 template "/etc/gemrc" do
   source "gemrc.erb"
+  mode "644"
+end
+
+node['gem-config']['users'].each do |user|
+  link "/etc/gemrc" do
+    to "/home/#{user}/.gemrc"
+  end
 end
